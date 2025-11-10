86a6bfc3012e9cd9c17db6dff7110d0a7ab8f034
Lineked <lineked@yandex.ru>
2025-11-10 20:11:57 +0300

    Added new route

--- webApp/src/jsMain/kotlin/com/cyberstarege/ruratesx/Main.kt
+++ webApp/src/jsMain/kotlin/com/cyberstarege/ruratesx/Main.kt
@@ -4,6 +4,8 @@ import app.softwork.routingcompose.HashRouter
 import app.softwork.routingcompose.Router
 import com.cyberstarege.ruratesx.components.MainContent
 import com.cyberstarege.ruratesx.di.initKoin
+import com.cyberstarege.ruratesx.pages.MainPage
+import com.cyberstarege.ruratesx.pages.MainViewModel
 import com.cyberstarege.ruratesx.styles.AppStylesheet
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
@@ -15,6 +17,9 @@ fun main() {
         MainContent {
             HashRouter(initPath = "/") {
                 val router = Router.current
+                route("/") {
+                    MainPage(mainViewModel = MainViewModel())
+                }
             }
         }
     }
